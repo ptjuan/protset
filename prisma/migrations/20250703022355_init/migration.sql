d3007e80ed4f082e5f29daf232de25be22b3a383
Juan Diego <juan.hebrath@bpt.com.co>
2025-07-02 22:32:30 -0500

    modelo de datos y migrado

---
+++ prisma/migrations/20250703022355_init/migration.sql
@@ -0,0 +1,78 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "cc" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("cc")
+);
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "cc" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("cc")
+);
+
+-- CreateTable
+CREATE TABLE "Informative" (
+    "id" SERIAL NOT NULL,
+    "product" TEXT NOT NULL,
+    "medium" TEXT NOT NULL,
+    "doneAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Informative_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "NonMonetaryTransactional" (
+    "id" SERIAL NOT NULL,
+    "product" TEXT NOT NULL,
+    "medium" TEXT NOT NULL,
+    "doneAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "NonMonetaryTransactional_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MonetaryTransactional" (
+    "id" SERIAL NOT NULL,
+    "product" TEXT NOT NULL,
+    "medium" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "doneAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "MonetaryTransactional_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" SERIAL NOT NULL,
+    "product" TEXT NOT NULL,
+    "medium" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
+    "doneAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "product" TEXT NOT NULL,
+    "medium" TEXT NOT NULL,
+    "doneAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "description" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MonetaryTransactional_category_key" ON "MonetaryTransactional"("category");
+
+-- AddForeignKey
+ALTER TABLE "Service" ADD CONSTRAINT "Service_user_fkey" FOREIGN KEY ("user") REFERENCES "User"("cc") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/app/api/route.ts
@@ -0,0 +1,10 @@
+import { prisma } from "@/libs/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+
+}
+
+export async function POST(){
+    
+}
